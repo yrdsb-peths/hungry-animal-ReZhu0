d264e0127b4387ae60799a06f7ffda55056c5bd6
ReZhu0 <117937494+ReZhu0@users.noreply.github.com>
2022-11-28 09:44:58 -0800

    Cat

---
+++ Cat.java
@@ -0,0 +1,20 @@
+/**
+ * Write a description of class Cat here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cat  
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Cat
+     */
+    public Cat()
+    {
+        
+    }
+
+}
