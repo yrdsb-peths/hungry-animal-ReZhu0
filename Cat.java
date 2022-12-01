dfe7ba247925a44fdb842f984e4b87427f3b4e87
ReZhu0 <117937494+ReZhu0@users.noreply.github.com>
2022-12-01 15:28:58 -0800

    cat is an elephant???

--- Cat.java
+++ Cat.java
@@ -12,6 +12,9 @@ public class Cat extends Actor
      * Act - do whatever the Cat wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    
     public void act()
     {
         // Add your action code here.
@@ -38,6 +41,7 @@ public class Cat extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.makeApple();
             world.increaseScore();
+            elephantSound.play();
         }
     }
     
