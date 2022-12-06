40de51347bfea2d355b93c4565c916c49dd4fa2c
ReZhu0 <117937494+ReZhu0@users.noreply.github.com>
2022-12-06 10:04:11 -0800

    He got CRUSHED

--- TitleScreen.java
+++ TitleScreen.java
@@ -6,19 +6,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Title extends World
+public class TitleScreen extends World
 {
     Label titleLabel = new Label("Hungry Cat", 70);
+    Label subTitle = new Label("(Hope he doesn't get crushed)", 30);
     /**
      * Constructor for objects of class Title.
      * 
      */
-    public Title()
+    public TitleScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         
-        addObject(titleLabel, 300, 200);
+        addObject(titleLabel, 300, 190);
+        addObject(subTitle, 300,250);
     }
     
     public void act()
