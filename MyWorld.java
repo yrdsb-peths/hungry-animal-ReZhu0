3417e941220a575738286a4fa8002d3e5c991b29
IyiAdeniyi <340925593@gapps.yrdsb.ca>
2022-12-14 13:25:21 -0500

    update comment

--- MyWorld.java
+++ MyWorld.java
@@ -1,8 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class MyWorld here.
- * 
+/**  
  * @author (your name) 
  * @version (a version number or a date)
  */
